60fe3ce8ed1b29858080573f883e0d9b66f6dd15
dipesheasemyai <dipesh@easemyai.com>
2026-04-09 10:56:58 +0530

    updated files

--- utils/Grad_Cam.py
+++ utils/Grad_Cam.py
@@ -7,7 +7,6 @@ from pytorch_grad_cam import EigenCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
 
-
 def Grad_Cam_process(model_path, image_path):
 
     class YOLOWrapper(nn.Module):
@@ -22,34 +21,40 @@ def Grad_Cam_process(model_path, image_path):
             return outputs
 
 
+    # Load model
     yolo = YOLO(model_path)
     yolo_model = yolo.model
     wrapped_model = YOLOWrapper(yolo_model)
 
-    target_layers = [yolo_model.model[-3]]
+    # MULTIPLE LAYERS (better representation)
+    target_layers = [
+        yolo_model.model[-1],
+        yolo_model.model[-2],
+        yolo_model.model[-3]
+    ]
 
+    # Load image
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    results = yolo(image_path)[0]
-
-
-    boxed_img = results.plot()  # already has boxes
-
-    boxed_img = cv2.cvtColor(boxed_img, cv2.COLOR_BGR2RGB)
-
-
-    img_norm = boxed_img.astype(np.float32) / 255.0
+    img_norm = img.astype(np.float32) / 255.0
 
     input_tensor = torch.from_numpy(img_norm).permute(2, 0, 1).unsqueeze(0)
 
+    # EigenCAM (PCA-based)
+    cam = EigenCAM(
+        model=wrapped_model,
+        target_layers=target_layers
+    )
 
-    cam = EigenCAM(model=wrapped_model, target_layers=target_layers)
     grayscale_cam = cam(input_tensor=input_tensor)[0]
 
     visualization = show_cam_on_image(img_norm, grayscale_cam, use_rgb=True)
 
-    cv2.imwrite("output/final_output6.jpg", cv2.cvtColor(visualization, cv2.COLOR_RGB2BGR))
+    cv2.imwrite("final_output7.jpg", cv2.cvtColor(visualization, cv2.COLOR_RGB2BGR))
+
 
 if __name__ == "__main__":
-    model_path = "/home/easemyai/dipesh_folder/runs/detect/train/weights/best.pt"
-    image_path = "/home/easemyai/dipesh_folder/task/dataset/images/val/hard_hat_workers260.png"
+    model_path = "/home/easemyai/Documents/mypycodes/dipesh_folder/face_detection_models/detection_model.pt"
+    image_path = "/home/easemyai/Documents/mypycodes/dipesh_folder/face_detection_models/unnamed.jpg"
     Grad_Cam_process(model_path, image_path)
